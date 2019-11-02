f657c67ee46fd22b1d7dca4d3cfea8cfb640c852
Adrian Figueroa Rodriguez <work.adrianfigueroa@gmail.com>
2019-11-02 02:19:01 +0000

    update

--- RSetup.R
+++ RSetup.R
@@ -1,61 +1,55 @@
 #Rsetup file
 
 #Checar si existe .Rprofile documento
-revisarRprofile <- function(){
-  if(file.exists(".Rprofile")){
-    checarRprofileSetup()
-  }else{
-    cat("Creando Archivo.\n")
-    file.create(file.path(getwd(), ".Rprofile"))
-    cat("Archivo creado.\n")
-    revisarRprofile()
-  }
-}
-
-#Leer archvio RprofileSetup.txt, transcribir lo a .Rprofile y resetear 
-#para que tomen efecto los cambios realizados.
-checarRprofileSetup <- function(){
+if(file.exists(".Rprofile")){
+  #Leer archvio RprofileSetup.txt, transcribir lo a .Rprofile y resetear 
+  #para que tomen efecto los cambios realizados.
   if(file.exists("RprofileSetup.txt")){
     source("~/RprofileSetup.txt")
-    cat("File already created/loaded\n")
-    RsetupFile <- readLines(
-      paste(getwd(), "/RprofileSetup.txt", sep = ""),
-      warn = FALSE)
+    cat("Archivo ya fue creado/cargado\n")
+    file.copy(from = "RprofileSetup.txt", to = ".Rprofile", overwrite = TRUE)
     cat("File read\n")
-    
   }else{
-    file.create("RprofileSetup.txt")
-    cat("File created\n")
-    RsetupFile <- readLines(
-      paste(getwd(), "/RprofileSetup.txt", sep = ""),
-      warn = FALSE)
-    cat("File read\n")
-    
+    file.choose()
+    cat("Archivo creado\n")
+    file.copy(from = "RprofileSetup.txt", to = ".Rprofile", overwrite = TRUE)
+    cat("Archivo leido")
+  }
+}else{
+  cat("Creando Archivo.\n")
+  file.create(file.path(getwd(), ".Rprofile"))
+  cat("Archivo creado.\n")
+  if(file.exists("RprofileSetup.txt")){
+    source("~/RprofileSetup.txt")
+    cat("Archivo ya fue creado/cargado\n")
+    file.copy(from = "RprofileSetup.txt", to = ".Rprofile", overwrite = TRUE)
+    cat("Archivo leido\n")
+  }else{
+    file.choose()
+    cat("Archivo creado\n")
+    file.copy(from = "RprofileSetup.txt", to = ".Rprofile", overwrite = TRUE)
+    cat("Archivo leido")
   }
 }
 
-file.copy(from = "RprofileSetup.txt", to = ".Rprofile", overwrite = TRUE)
-.rs.api.restartSession()
+.rs.restartR()
 
 #Cargar librerias requeridas
 cargarlibrerias <- function(){
   paqueteriasRequeridas <- c("twitteR", "rtweet", "influenceR", "igraph", "SentimentAnalysis", "syuzhet", "quanteda")
   
   tryCatch({
-    for(i in 1:length(paqueteriasRequeridas)){
-      
-      #check if the paqueteria is installed not loaded, it won't be loaded
-      if(require(paqueteriasRequeridas[i],character.only = TRUE)){
-        cat(sub(pattern = "[i]", replacement = paqueteriasRequeridas[i], x = "Paquete [i] ha sido cargado.\n"))
-      }else{
-        cat(sub(pattern = "[i]", replacement = paqueteriasRequeridas[i], x = "Paquete [i] no esta cargado.\n"))
-        install.packages(paqueteriasRequeridas[i])
-        cargarlibrerias()
+      for(i in 1:length(paqueteriasRequeridas)){
+        
+        #check if the paqueteria is installed not loaded, it won't be loaded
+        if(require(paqueteriasRequeridas[i],character.only = TRUE)){
+          cat(sub(pattern = "[i]", replacement = paqueteriasRequeridas[i], x = "Paquete [i] ha sido cargado.\n"))
+        }else{
+          cat(sub(pattern = "[i]", replacement = paqueteriasRequeridas[i], x = "Paquete [i] no esta cargado.\n"))
+          install.packages(paqueteriasRequeridas[i])
+          cargarlibrerias()
+        }
       }
     }
-  },
-  finally = {
-    "Librerias cargadas"
-  }
   )
 }
